13c65f011d6b99f9fccd24f7d38088790dd99762
Santiago <santiago.valle1245@gmail.com>
2026-01-29 15:06:20 -0600

    Mostrar el index en el servidor =)

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
 //Importar el modulo HTTP
 const http = require('http');
 
+const fs = require('fs');
 //Definir el puerto en el que se ejecutara el servidor
 const PORT = 3000;
 
 //Crear el servidor
 const servidor = http.createServer((req, res) => {
 
-//Manejo de diferentes rutas
-    if(req.url === '/'){
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('<h1>Pagina de principal.</h1>');
-    } 
-    else if(req.url === '/acerca'){
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('<p>Esta es la pagina de acerca de.</p>');
-    }
-    else {
-    res.writeHead(404, { 'Content-Type': 'text/html' });
-    res.end('<h1>Pagina no encontrada.</h1>');
-    }
+    //Lectura asincrona: no bloquea el Evento Loop
+    fs.readFile('./index.html', (err, data) => {
+        if (err) {
+            res.statusCode = 500;
+            res.end('Error al leer el archivo');;
+            return;
+        }
+
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.end(data);
+    })
 });
 
 //Iniciar el servidor
